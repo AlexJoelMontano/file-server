c581cf142a7765a80341198fa6d53eb2df0d1241
AlexJoelMontano <alexjoelmontano@gmail.com>
2022-11-24 15:50:47 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# file-server
+ Project for creating a functional file server website.
---
+++ script.js
@@ -0,0 +1,17 @@
+
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+    if (uname === "alexjoelmontano@gmail.com" || uname === "Alexjoelmontano@gmail.com") {
+        alert("Welcome");
+    }
+    else {
+        alert("Unknown Username or Password");
+    }
