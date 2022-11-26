72b5224c55dd79863e6278c28514294ac9b0ad89
AlexJoelMontano <alexjoelmontano@gmail.com>
2022-11-25 22:48:08 -0500

    Created hub page
    added link to button to reach hub page.
    created new dynamic headers for hub page.
    created dynamic header template in hub page to ensure uniform look
    added js code to change background with nav selection.
    added nav selection highlight in js.

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
 
 // Get the modal
-var modal = document.getElementById('id01');
+let modal = document.getElementById('id01');
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
+}
+function openNav(navName, elmnt, color) {
+  // Hide all elements with class="tabcontent" by default */
+  let i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+}
+
+  // Remove the background color of all tablinks/buttons
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+  tablinks[i].style.backgroundColor = "";
+}
+
+  // Show the specific tab content
+  document.getElementById(navName).style.display = "flex";
+
+  // Add the specific color to the button used to open the tab content
+  elmnt.style.backgroundColor = color;
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click(); 
+
+
+let login = document.getElementsByClassName("login-button");
+let user = document.getElementsByClassName("uname");
+
+if (user === null && login === true){
+    alert("Please enter a username");
 }
